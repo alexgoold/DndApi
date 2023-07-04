809f8fef429f9bc8b9336f11e8f04b8ff5350c47
alexgoold <102036021+alexgoold@users.noreply.github.com>
2023-07-04 22:21:17 +0200

    Testing github Activity

--- Client/Program.cs
+++ Client/Program.cs
@@ -13,6 +13,7 @@ namespace DndApi.Client
 			builder.RootComponents.Add<App>("#app");
 			builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
 			builder.Services.AddHttpClient("DndApi.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
 				.AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
