c34c4083fa9ead05a259725d5ef8c479e4193637
alexgoold <102036021+alexgoold@users.noreply.github.com>
2023-07-04 22:22:02 +0200

    Remove Comments

--- Client/Program.cs
+++ Client/Program.cs
@@ -12,12 +12,8 @@ namespace DndApi.Client
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
 			builder.RootComponents.Add<App>("#app");
 			builder.RootComponents.Add<HeadOutlet>("head::after");
-
-
 			builder.Services.AddHttpClient("DndApi.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
 				.AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-
-			// Supply HttpClient instances that include access tokens when making requests to the server project
 			builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("DndApi.ServerAPI"));
 
 			builder.Services.AddApiAuthorization();
